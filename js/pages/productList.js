9793d9560032cfc4b0b6b1237e0034e1e8b5cc79
s0y0ungk <soyo2039@gmail.com>
2026-06-18 18:16:01 +0900

    푸터 해더 추가

--- js/pages/productList.js
+++ js/pages/productList.js
@@ -1,3 +1,11 @@
+import { getFilteredStores, getSidoList, getSigunguList } from "../common.js";
+import { renderHeader } from "../modules/header.js";
+import { renderFooter } from "../modules/footer.js";
+document.addEventListener("DOMContentLoaded", () => {
+  renderHeader();
+  renderFooter();
+});
+
 const sortButtons = document.querySelectorAll(".filter-btn");
 const countPerPage = 12;
 const moreBtn = document.querySelector(".more-btn");
@@ -113,16 +121,16 @@ sortButtons.forEach(button => {
 function renderProducts(products) {
   const productList = document.querySelector(".sunglasses-page");
   // 배너 랜덤
-const randomIndex1 =
-  Math.floor(Math.random() * bannerImages.length);
-let randomIndex2;
-do {
-  randomIndex2 =
+  const randomIndex1 =
     Math.floor(Math.random() * bannerImages.length);
-} while (randomIndex1 === randomIndex2);
+  let randomIndex2;
+  do {
+    randomIndex2 =
+      Math.floor(Math.random() * bannerImages.length);
+  } while (randomIndex1 === randomIndex2);
 
-const bannerImage1 = bannerImages[randomIndex1];
-const bannerImage2 = bannerImages[randomIndex2];
+  const bannerImage1 = bannerImages[randomIndex1];
+  const bannerImage2 = bannerImages[randomIndex2];
 
   let visibleProducts;
 
@@ -403,9 +411,9 @@ function renderPagination() {
     });
 
 
-  }
+}
 
-    prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener("click", () => {
   if (currentPage > 1) {
     currentPage--;
     renderProducts(filteredData);
@@ -432,4 +440,4 @@ nextBtn.addEventListener("click", () => {
   }
 });
 
-  fetchProducts();
+fetchProducts();
